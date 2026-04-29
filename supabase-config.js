7f3a297ea17a419f728e342cbc2f74af13d92979
Eryk <erykverhagen@users.noreply.github.com>
2026-04-29 16:01:14 +0200

    v1

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,5 @@
+// Dead Wax Supabase config
+// This file is safe to publish. Do NOT put service_role keys here.
 window.DEAD_WAX_SUPABASE = {
   url: 'https://nptphtsiborghjlzetbr.supabase.co',
   anonKey: 'sb_publishable_7Z13DX675sA3GcLNCJpI1w_wEk09ZyG'
